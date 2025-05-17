91b3a4cbed7fd8f1cbcd799057851166dcc12834
Nipun Kumar <111305023+NipunRaj96@users.noreply.github.com>
2025-05-17 18:26:03 +0530

    python fundamentals for beginner

---
+++ basic_py_1.py
@@ -0,0 +1,77 @@
+# name = input("Enter your name : ")
+# print("Good Morning"Nipun
+#       , name)
+
+
+# li = [1,6,7,4,3,9,5]
+# li.sort()
+# print(li)
+# li.reverse()
+# print(li)
+# print(li)
+# li.append(10)
+# print(li)
+
+
+# a = ("Harry")
+# print(type(a))
+# b = ("NIPUN","HARRY","VIJAY")
+# print(type(b))
+# c = [ "NIPUN" , 1 , 5 ,7 , "VIJAY"]
+# print(type(c)) 
+
+
+# s =(1,4,5,3,5,7,2,3,4,5,6,9,0,7,8,6,6,1,32,65,54,32,99,64,12,20)
+# print(s)
+# #cannot access elements by index
+
+
+#conditional statements
+# a = int(input("Enter your age :"))
+# if(a>=18):
+#     print("yes,dede vote")
+# else:
+#     print("No,ghr ja")
+
+# print("Thank you for the service")
+  
+
+# #loops
+# i = 1
+# while(i<9):
+#     print(i)
+#     i += 1 #i = i+1 is the same thing
+# l=[1,2,3,4,5,6,7,8,"NIPUN","HArry"] #for loop for lists
+# for i in (l):
+#    print(i)
+# t = (1,2,3,4,"nipun","harry") #for loop for tuples
+# for i in (t):
+#     print(i)
+# string = "Harry" #for loop for string
+# for i in (string):
+#     print(i)
+
+
+# l = (1,2,3,4,5,6,7,"nipun","harry")
+# print(len(l))
+
+
+#break,continue,pass
+# for i in range (100):
+#     if(i == 50):
+#        break
+#     print(i)
+
+
+# for i in range (100):
+#     if (i==50):
+#         continue
+#     print(i)
+
+#function and recursion
+def greet():
+    n = input("Enter your name: ")
+    print("Good Morning",n)
+
+greet()  # function call
+ 
